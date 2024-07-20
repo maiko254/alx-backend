b014c81f5f7942f613553f5f53b23ba294d7b552
maiko254 <bonyomichael406.com>
2024-07-20 09:13:36 +0300

    Implemented a helper function returining a tuple with start and end range of indexes in a list to be used as pagination parameters

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module implementing a helper function to get the range of indexes in a list
+for pagination
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    particular pagination parameters."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return (start_index, end_index)
