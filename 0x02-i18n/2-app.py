69306a9b6a7fe8dd8ce053d561cad6ea2d304269
maiko254 <bonyomichael406.com>
2024-07-30 14:00:27 +0300

    Flask app with Babel object that determines best match of supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+def get_locale():
+    """Get the locale from the request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel = Babel(app, default_locale='en', default_timezone='UTC',
+              locale_selector=get_locale)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+
+@app.route('/')
+def index():
+    """Render the index.html template"""
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
