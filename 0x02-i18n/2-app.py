be01660774eade3ed0015d4fce512b4499c8e7e4
maiko254 <bonyomichael406.com>
2024-07-30 22:31:34 +0300

    Correction: Fixed code to be compatible with flask-babel 2.0

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-
-
-def get_locale():
-    """Get the locale from the request"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel = Babel(app, default_locale='en', default_timezone='UTC',
-              locale_selector=get_locale)
-
 
 class Config:
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get the locale from the request"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
@@ -24,4 +26,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
