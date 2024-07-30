943f114f6a1faa0134d8c686ab6813000de29ffa
maiko254 <bonyomichael406.com>
2024-07-30 21:39:11 +0300

    Correction

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,18 +1,14 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-
-
-def get_locale():
-    """Get the locale from the request"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel = Babel(app, default_locale='en', default_timezone='UTC')
 
 class Config:
     LANGUAGES = ["en", "fr"]
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, default_locale='en', default_timezone='UTC')
 
 
 @app.route('/')
