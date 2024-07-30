0528c7e06ca94f53c8573ffc1a333317529a0f4d
maiko254 <bonyomichael406.com>
2024-07-30 22:43:29 +0300

    Correction: Compatibility with flask-babel version 2.0

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -3,7 +3,16 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
+
+class Config:
+    """Supported languages definitions"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app, default_locale='en', default_timezone='UTC')
 
 
@@ -16,22 +25,11 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-class Config:
-    """Supported languages definitions"""
-    LANGUAGES = ["en", "fr"]
-
-
-app.config.from_object(Config)
-
-
 @app.route('/')
 def index():
     """Render the index.html template"""
-    title = _('Welcome to Holberton')
-    header = _('Hello world!')
-    return render_template('4-index.html',
-                           home_title=title, home_header=header)
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
