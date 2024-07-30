fe74b6c785e84a27484286c86117609b69b93f83
maiko254 <bonyomichael406.com>
2024-07-30 21:35:48 +0300

    Correction: compatibility with flask_babel 2.0

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -9,9 +9,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel = Babel(app, default_locale='en', default_timezone='UTC',
-              locale_selector=get_locale)
-
+babel = Babel(app, default_locale='en', default_timezone='UTC')
 
 class Config:
     LANGUAGES = ["en", "fr"]
