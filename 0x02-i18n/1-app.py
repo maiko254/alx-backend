c647762bb86562bfc60a9e299c8eb782395bea14
maiko254 <bonyomichael406.com>
2024-07-30 21:41:10 +0300

    Correction

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,11 +4,13 @@ from flask_babel import Babel
 
 class Config:
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app, default_locale='en', default_timezone='UTC')
+babel = Babel(app)
 
 
 @app.route('/')
