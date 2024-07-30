5ef5d783a193dbc9f43fda7992f99678735d8622
maiko254 <bonyomichael406.com>
2024-07-30 22:38:29 +0300

    Correction

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -3,8 +3,17 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
+
+class Config:
+    """Class defining supported languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
-babel = Babel(app, default_locale='en', default_timezone='UTC')
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
@@ -13,22 +22,11 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-class Config:
-    """Class defining supported languages"""
-    LANGUAGES = ["en", "fr"]
-
-
-app.config.from_object(Config)
-
-
 @app.route('/')
 def index():
     """Render the index.html template"""
-    title = _('Welcome to Holberton')
-    header = _('Hello world!')
-    return render_template('3-index.html',
-                           home_title=title, home_header=header)
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port='5000')
